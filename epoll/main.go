cc3598ae852360d21e31a634c293bf787b4c98c6
vincentiusvin <vincentius_vin@yahoo.com>
2025-06-14 18:33:56 +0700

    refactor: reorganize epoll code

--- epoll/main.go
+++ epoll/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	lowlevel "learn_io/low-level"
-	"log"
 
 	"golang.org/x/sys/unix"
 )
@@ -13,8 +12,7 @@ func main() {
 }
 
 type server struct {
-	port      int
-	connQueue int
+	port int
 
 	sockFD   lowlevel.SockFD
 	sockEpFD int
@@ -22,8 +20,7 @@ type server struct {
 
 func NewServer(port int) *server {
 	return &server{
-		port:      8000,
-		connQueue: 1,
+		port: 8000,
 	}
 }
 
@@ -34,53 +31,56 @@ func (s *server) Listen() error {
 	}
 	sock.SetNonblock(true)
 	s.sockFD = sock
-	s.pollForConnections()
-	return nil
-}
 
-func (s *server) pollForConnections() error {
-	epfd, err := unix.EpollCreate1(0)
+	epfd, err := setupEpoll(int(sock), unix.EPOLLIN)
 	if err != nil {
 		return err
 	}
 	s.sockEpFD = epfd
+	return nil
+}
 
-	ev := new(unix.EpollEvent)
-	ev.Events = unix.EPOLLIN
-	ev.Fd = int32(s.sockFD)
-	if err = unix.EpollCtl(epfd, unix.EPOLL_CTL_ADD, int(s.sockFD), ev); err != nil {
+func (s *server) Accept() error {
+	connQueue := 10
+	evs := make([]unix.EpollEvent, connQueue)
+
+	n, err := unix.EpollWait(s.sockEpFD, evs, 10)
+	if err != nil {
 		return err
 	}
 
-	num := 1
-	evs := make([]unix.EpollEvent, num)
-	for {
-		log.Printf("Starting to wait...")
-		n, err := unix.EpollWait(s.sockEpFD, evs, -1)
-		if err != nil {
-			break
+	for _, event := range evs[:n] {
+		currFd := lowlevel.SockFD(event.Fd)
+		if currFd != s.sockFD {
+			continue
 		}
-
-		log.Printf("Got %v events", n)
-
-		for _, event := range evs[:n] {
-			currFd := lowlevel.SockFD(event.Fd)
-			if currFd != s.sockFD {
-				continue
-			}
-			s.handleNewConnection(currFd)
-		}
+		s.handleNewConnection(currFd)
 	}
+
 	return nil
 }
 
 func (s *server) handleNewConnection(sockFd lowlevel.SockFD) {
-	connFd, conn, err := sockFd.AcceptConnection()
+	connFd, _, err := sockFd.AcceptConnection()
 	if err != nil {
 		panic(err)
 	}
-
-	log.Printf("Got connection %v:%v", conn.Addr, conn.Port)
-
 	connFd.SetNonblock(true)
+
+	epfd, err := setupEpoll(int(connFd), unix.EPOLLIN)
+}
+
+func setupEpoll(fd int, events uint32) (int, error) {
+	epfd, err := unix.EpollCreate1(0)
+	if err != nil {
+		return 0, err
+	}
+
+	ev := new(unix.EpollEvent)
+	ev.Events = events
+	ev.Fd = int32(fd)
+	if err = unix.EpollCtl(epfd, unix.EPOLL_CTL_ADD, fd, ev); err != nil {
+		return 0, err
+	}
+	return epfd, nil
 }
