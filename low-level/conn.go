759f1a13fbbc0f47f8fd8184ae9b0eb05fec96f2
vincentiusvin <vincentius_vin@yahoo.com>
2025-06-14 16:35:37 +0700

    feat: add closing

--- low-level/conn.go
+++ low-level/conn.go
@@ -1,18 +1,21 @@
 package lowlevel
 
-import "syscall"
+import (
+	"io"
+	"syscall"
+)
 
 type SockFD int
 type ConnFD int
 
-func OpenSocket() (SockFD, error) {
-	fd, err := syscall.Socket(syscall.AF_INET, syscall.SOCK_STREAM|syscall.SOCK_NONBLOCK, 0)
+func OpenSocket(port int) (SockFD, error) {
+	fd, err := syscall.Socket(syscall.AF_INET, syscall.SOCK_STREAM, 0)
 	if err != nil {
 		return 0, err
 	}
 
 	sa := new(syscall.SockaddrInet4)
-	sa.Port = 8000
+	sa.Port = port
 	sa.Addr = [...]byte{0, 0, 0, 0}
 	if err = syscall.Bind(fd, sa); err != nil {
 		return 0, err
@@ -25,6 +28,10 @@ func OpenSocket() (SockFD, error) {
 	return SockFD(fd), nil
 }
 
+func (fd SockFD) Close() error {
+	return syscall.Close(int(fd))
+}
+
 func (fd SockFD) AcceptConnection() (ConnFD, *syscall.SockaddrInet4, error) {
 	// this will immediately return if we set syscall.SOCK|NONBLOCK above and we don't have anything queued.
 	// fun!
@@ -34,4 +41,18 @@ func (fd SockFD) AcceptConnection() (ConnFD, *syscall.SockaddrInet4, error) {
 	}
 	sa4 := sa.(*syscall.SockaddrInet4)
 	return ConnFD(nfd), sa4, nil
+}
+
+var _ io.ReadWriter = ConnFD(0)
+
+func (cd ConnFD) Read(b []byte) (int, error) {
+	return syscall.Read(int(cd), b)
+}
+
+func (cd ConnFD) Write(b []byte) (int, error) {
+	return syscall.Write(int(cd), b)
+}
+
+func (cd ConnFD) Close() error {
+	return syscall.Close(int(cd))
 }
--- low-level/main/main.go
+++ low-level/main/main.go
@@ -1,18 +1,30 @@
 package main
 
 import (
-	"fmt"
 	lowlevel "learn_io/low-level"
+	"log"
 )
 
 func main() {
-	fd, err := lowlevel.OpenSocket()
+	fd, err := lowlevel.OpenSocket(8000)
 	if err != nil {
 		panic(err)
 	}
+	defer fd.Close()
+
 	nfd, conn, err := fd.AcceptConnection()
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(nfd, conn)
+	defer nfd.Close()
+
+	log.Printf("Got connection from: %v:%v", conn.Addr, conn.Port)
+	for {
+		b := make([]byte, 1024)
+		n, err := nfd.Read(b)
+		if err != nil {
+			break
+		}
+		log.Print(string(b[:n]))
+	}
 }
