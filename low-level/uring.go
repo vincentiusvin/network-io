7600a48142539dd654564874ca5856a3b0543d59
vincentiusvin <vincentius_vin@yahoo.com>
2025-06-28 23:21:54 +0700

    feat: implement uring setup

--- low-level/uring.go
+++ low-level/uring.go
@@ -6,6 +6,36 @@ import (
 	"golang.org/x/sys/unix"
 )
 
+const (
+	IORING_FEAT_SINGLE_MMAP     = 1 << 0
+	IORING_FEAT_NODROP          = 1 << 1
+	IORING_FEAT_SUBMIT_STABLE   = 1 << 2
+	IORING_FEAT_RW_CUR_POS      = 1 << 3
+	IORING_FEAT_CUR_PERSONALITY = 1 << 4
+	IORING_FEAT_FAST_POLL       = 1 << 5
+	IORING_FEAT_POLL_32BITS     = 1 << 6
+	IORING_FEAT_SQPOLL_NONFIXED = 1 << 7
+	IORING_FEAT_EXT_ARG         = 1 << 8
+	IORING_FEAT_NATIVE_WORKERS  = 1 << 9
+	IORING_FEAT_RSRC_TAGS       = 1 << 10
+	IORING_FEAT_CQE_SKIP        = 1 << 11
+	IORING_FEAT_LINKED_FILE     = 1 << 12
+	IORING_FEAT_REG_REG_RING    = 1 << 13
+	IORING_FEAT_RECVSEND_BUNDLE = 1 << 14
+	IORING_FEAT_MIN_TIMEOUT     = 1 << 15
+	IORING_FEAT_RW_ATTR         = 1 << 16
+	IORING_FEAT_NO_IOWAIT       = 1 << 17
+)
+
+const (
+	IORING_OFF_SQ_RING    uint64 = 0
+	IORING_OFF_CQ_RING    uint64 = 0x8000000
+	IORING_OFF_SQES       uint64 = 0x10000000
+	IORING_OFF_PBUF_RING  uint64 = 0x80000000
+	IORING_OFF_PBUF_SHIFT        = 16
+	IORING_OFF_MMAP_MASK  uint64 = 0xf8000000
+)
+
 // see man io_uring_setup (2)
 
 type IOUringParams struct {
@@ -43,7 +73,18 @@ type IOCQringOffsets struct {
 	Resv         [3]uint32
 }
 
-func IOUringEnter(entries uint32, params *IOUringParams) (fd int, err error) {
+const (
+	SizeOfIOUringCQE = unsafe.Sizeof(IOUringCQE{})
+	SizeOfUint32     = unsafe.Sizeof(uint32(0))
+)
+
+type IOUringCQE struct {
+	User_data uint64
+	Res       int32
+	Flags     uint32
+}
+
+func IOUringSetup(entries uint32, params *IOUringParams) (fd int, err error) {
 	paramsPtr := unsafe.Pointer(params)
 	r0, _, e1 := unix.Syscall(unix.SYS_IO_URING_SETUP, uintptr(entries), uintptr(paramsPtr), 0)
 	if e1 == 0 {
--- uring/main.go
+++ uring/main.go
@@ -3,13 +3,50 @@ package main
 import (
 	lowlevel "learn_io/low-level"
 	"log"
+
+	"golang.org/x/sys/unix"
 )
 
 func main() {
 	params := new(lowlevel.IOUringParams)
-	fd, err := lowlevel.IOUringEnter(10, params)
+	fd, err := lowlevel.IOUringSetup(10, params)
 	if err != nil {
 		panic(err)
 	}
-	log.Print(fd)
+
+	sq, err := mmapSQ(fd, params)
+	if err != nil {
+		panic(err)
+	}
+	cq, err := mmapCQ(fd, params)
+	if err != nil {
+		panic(err)
+	}
+	log.Println(sq, cq)
+}
+
+func mmapSQ(ringFD int, params *lowlevel.IOUringParams) ([]byte, error) {
+	sqRingLen := params.Sq_off.Array + params.Sq_entries*uint32(lowlevel.SizeOfUint32)
+
+	// Go's mmap(2) is different from C so be careful!
+	return unix.Mmap(
+		ringFD,
+		int64(lowlevel.IORING_OFF_SQ_RING),
+		int(sqRingLen),
+		unix.PROT_READ|unix.PROT_WRITE,
+		unix.MAP_SHARED|unix.MAP_POPULATE,
+	)
+}
+
+func mmapCQ(ringFD int, params *lowlevel.IOUringParams) ([]byte, error) {
+	cqRingLen := params.Cq_off.Cqes + params.Cq_entries*uint32(lowlevel.SizeOfIOUringCQE)
+
+	// Go's mmap(2) is different from C so be careful!
+	return unix.Mmap(
+		ringFD,
+		int64(lowlevel.IORING_OFF_CQ_RING),
+		int(cqRingLen),
+		unix.PROT_READ|unix.PROT_WRITE,
+		unix.MAP_SHARED|unix.MAP_POPULATE,
+	)
 }
